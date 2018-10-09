8606600a271f28812d4b74a9f16a5ceba31e63a2
joasp591 <joasp591@student.liu.se>
2018-10-09 16:19:55 +0200

    moved name of streamer and started a thumbnaildiv

---
+++ src/molecules/ThumbnailWindow.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+import '../css/ThumbnailWindow.css';
+
+var StreamName = 'riotgames';
+class InfoWindow extends Component {
+  render() {
+    
+    return (
+      
+      <div className="Thumbnail-window-holder">
+
+        <div className="Thumbnail-holder">
+        
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default InfoWindow;
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import '../css/GamePage.css';
 import MediaWindow from '../molecules/MediaWindow.js';
 import InfoWindow from '../molecules/InfoWindow.js';
-
+import ThumbnailWindow from '../molecules/ThumbnailWindow.js';
 class GamePage extends Component {
     
     render() {
         return(
             <div className="game-page-container">
-            <MediaWindow />
-            <InfoWindow />
-            
+                <MediaWindow />
+                <InfoWindow />
+                <ThumbnailWindow />
             </div>
 
         )
