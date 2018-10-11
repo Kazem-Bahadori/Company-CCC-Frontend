db3db9f75224796adfdf82d558c8a9b4e419c60a
joasp591 <joasp591@student.liu.se>
2018-10-11 16:15:19 +0200

    Moved state to gamepage instead of mediawindow

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -3,12 +3,40 @@ import '../css/GamePage.css';
 import MediaWindow from '../molecules/MediaWindow.js';
 import InfoWindow from '../molecules/InfoWindow.js';
 import ThumbnailWindow from '../molecules/ThumbnailWindow.js';
+
+
+let streamerId = '';
 class GamePage extends Component {
+    
+      state = {
+        streamName: '',
+      }
+
+      componentDidMount() {
+
+        const currentStream = "http://localhost:8080/api/twitch/filters?filterType=streams&additionalFilter=game_id&amount=" + this.props.gameId
+        fetch(currentStream, {headers: {"Client-ID": '3jxj3x3uo4h6xcxh2o120cu5wehsab'}}) 
+              //Convert response into json. /Johandg
+              .then(response => response.json())
+              //Loop through the JSON-array to grab each individual element and place inside the popularGameArray state. /Johandg
+              .then(response => {
+                
+                streamerId = response.data[0].user_id;
+                
+                const streamerName = "http://localhost:8080/api/twitch/filters?filterType=users&additionalFilter=id&amount=" + streamerId
+                  fetch(streamerName, {headers: {"Client-ID": '3jxj3x3uo4h6xcxh2o120cu5wehsab' }})
+                      .then(response => response.json())
+                      .then(response => {
+              
+                      this.setState({streamName: response.data[0].login})
+                  })
+              })
+      }
     
     render() {
         return(
             <div className="game-page-container">
-                <MediaWindow gameId={this.props.gameId} />
+                <MediaWindow streamName={this.state.streamName} />
                 <InfoWindow />
                 <ThumbnailWindow />
             </div>
