79470f117669f2548ce3cc6b30fc35798489a939
Herman Svensk <herman.svensk@gmail.com>
2018-10-16 14:56:14 +0200

    Added first test for react component

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import '../css/Button.css';
 
 class Button extends Component {
-    
+
     render() {
       return (
         <div className="button" onClick={this.props.onClick}>
--- test/appTest.js
+++ test/appTest.js
@@ -1,15 +1,33 @@
 const assert = require('chai').assert; //bringring in the chai library
+const expect = require('chai').expect;
 const button = require('../src/atoms/Button'); //Bringing in the app from the app.js, i.e. the file we want to test
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import Button from '../src/atoms/Button';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 
 describe('FR002', () =>{
   it('Button should not be NULL', ()=> { //first parameter is the description, the second parameter is
     //assert.isEmpty(button); //first parameter is the file being tested (the paraenthesis is because app is returning a function!)
     //the second parameter is what it is supposed to return
-    assert.isNotNull(button); 
-    
+    assert.isNotNull(button);
     
   });
 }); //App is what we name the file we're testing, here app.js. local variable
+
+describe('Test button class', () =>{
+  it('Button should have class button', () => { //first parameter is the description, the second parameter is
+    //assert.isEmpty(button); //first parameter is the file being tested (the paraenthesis is because app is returning a function!)
+    //the second parameter is what it is supposed to return
+    let btn = shallow(<Button />);
+    expect(btn.hasClass('button')).to.equal(true);
+  });
+});
 
 //npm run test_case -s To run the test
 
