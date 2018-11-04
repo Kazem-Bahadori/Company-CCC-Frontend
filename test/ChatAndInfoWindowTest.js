a67fb810f45f08dcc34e45e541d931b89dddf3f0
lintu069 <lintu069@student.liu.se>
2018-11-04 17:09:31 +0100

    Added more test cases to ChatAndInfoWindow

--- test/ChatAndInfoWindowTest.js
+++ test/ChatAndInfoWindowTest.js
@@ -11,18 +11,25 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 
-describe('FR029: Test ChatAndInfoWindow class', () =>{
-  it('ChatAndInfoWindow should have a buy button', () => {
-    //const state = {
-    //        showChat: true,
-  //          showInfo: false
-  //  }
-    //let wrapper = shallow(<ChatAndInfoWindow/>, {state}).dive({state});
-    //console.log(wrapper.state());
-    //wrapper.find('Button').simulate('click');//it finds Button the InfoWindow class
+describe('FR062: Test ChatAndInfoWindow class', () =>{
+  it('ChatAndInfoWindow should display chat and hide info at loading', () => {
+    let wrapper = shallow(<ChatAndInfoWindow/>);
+    expect(wrapper.state('showChat')).to.equal(true);
+    expect(wrapper.state('showInfo')).to.equal(false);
+    expect(wrapper.find('.button-style')).to.have.length(2);
+    //TODO: write tests for simulating clicks and checking text on Chat & Game Info-buttons (same class)
+    //expect(wrapper.find('.button-style')[0].text()).to.equal(' Chat ');//.simulate('click');//.to.equal(' Chat ');
   });
 });
 
+describe('FR029: Test ChatAndInfoWindow class', () =>{
+  it('ChatAndInfoWindow should have a buy button', () => {
+    let wrapper = shallow(<ChatAndInfoWindow/>);
+    //expect(wrapper.find('button')).to.have.length(3);
+    expect(wrapper.find('.buy-on-steam-btn').text()).to.equal(' Buy on Steam ');
+    wrapper.find('.buy-on-steam-btn').simulate('click');
+  });
+});
 
 //tests of different imports, seems like components with state variables can't be imported? Compiler problem, can be solved with jest/babel/changed babelrc?
 //const thumbnail = require('../src/atoms/Thumbnail.js'); [works]
