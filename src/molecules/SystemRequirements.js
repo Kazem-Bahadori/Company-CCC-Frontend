16d8b61e120fc5f6175c545b8c5caa583405fc3f
JOHAN DE GROOT <johde234@student.liu.se>
2018-11-07 16:53:55 +0100

    Can now show minimum requirements for Steam games

--- src/molecules/SystemRequirements.js
+++ src/molecules/SystemRequirements.js
@@ -4,14 +4,36 @@ import Button from '../atoms/Button.js';
 
 
 class SystemRequirements extends Component {
-  render() {
+
+  state = {
+    miniReq: '',
+  }
+
+  componentDidMount() {
     
+    console.log("system req steamId: " + this.props.steamId)
+    if (this.props.steamId !== undefined) {
+    let fetchSystemReq = "http://localhost:8080/api/steam/filters?assetType=system_requirements&filterType=app_id&filterValue=" + this.props.steamId
+    fetch(fetchSystemReq)
+    .then(response => response.json())
+    .then(response => {
+      console.log("system req response: " + response.pc_requirements.minimum)
+      this.setState({ miniReq: response.pc_requirements.minimum })
+      
+    })
+  } else {
+    this.setState({ miniReq: "Sorry we can't show you the requirements for this game!" })
+  }
+
+  
+  }
+  render() {
     return (
       
       <div className="Info-window-holder">
         <div className="Name-holder">
-           SYSTEM REQ
-            
+           Minimum Requirements
+           <p> {this.state.miniReq} </p>
 
         </div>
       </div>
