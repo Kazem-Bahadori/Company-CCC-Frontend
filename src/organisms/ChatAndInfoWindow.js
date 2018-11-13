3814d3452cdcbde76926cbefb44d2898066f0b17
JOHAN DE GROOT <johde234@student.liu.se>
2018-11-13 11:00:59 +0100

    Code skeleton for reviews, forgot to make branch

--- src/organisms/ChatAndInfoWindow.js
+++ src/organisms/ChatAndInfoWindow.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TwitchChat from '../molecules/TwitchChat.js';
-import InfoWindow from '../molecules/InfoWindow.js';
+import ReviewWindow from '../molecules/ReviewWindow';
 import SystemRequirements from '../molecules/SystemRequirements.js';
 import '../css/ChatAndInfoWindow.css'
 import steamlogo from '../images/steamlogo.png';
@@ -61,7 +61,7 @@ class ChatAndInfoWindow extends Component {
             return <TwitchChat streamName={this.props.streamName} />;
 
             case "Reviews":
-            return <InfoWindow streamName={this.props.streamName} viewers={this.props.viewers}/>;
+            return <ReviewWindow steamId={steamId} streamName={this.props.streamName} viewers={this.props.viewers}/>;
 
             case "System Requirements":
             return <SystemRequirements steamId={steamId} />
