ada2c3119174c43029711752bc67cbc4b962ab4d
gullis phung <gulph714@student.liu.se>
2018-11-19 09:40:10 +0100

    possible to search for streams and games

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,37 +1,66 @@
 import React from 'react';
 import Searchbar from '../molecules/Searchbar';
 
-let starterString = 'http://localhost:8080/api/twitch/search?assetType=games&queryString=';
+let starterStringGames = 'http://localhost:8080/api/twitch/search?assetType=games&queryString=';
+let starterStringStreamers = 'http://localhost:8080/api/twitch/search?assetType=streams&queryString=';
 class SearchPage extends React.Component {
 
     state = {
-        gameArray:[]
+        gameArray:[],
+        streamerArray: []
     };    
 
-    fetchFromBackend = (value) => {
-        if(value.length <1){
-            return;
-        } 
+    fetchGamesFromBackend = (value) => { 
         this.setState({ gameArray: [] });
 
-        fetch(starterString.concat(value), {})
+        fetch(starterStringGames.concat(value), {})
         .then(response => response.json())
         .then(response => {
+            console.log(response)
             if (!response.games) return;
             response.games.map((index) =>
-            this.setState({ gameArray: [...this.state.gameArray, index] }),
-            )    
-          })
+                this.setState({ gameArray: [...this.state.gameArray, index] }),
+            )        
+        })
+    }
+
+    fetchStreamersFromBackend = (value) => {
+        this.setState({ streamerArray: [] });
+        fetch(starterStringStreamers.concat(value), {})
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (!response.streams) return;
+            response.streams.map((index) =>
+                this.setState({ streamerArray: [...this.state.streamerArray, index] }),
+            )                        
+        })
+    }
+    handleInputChars = (inputValue) => {
+        return (
+            inputValue = inputValue
+            .replace(/å/g, '_x00e5_')
+            .replace(/Å/g, '_x00c5_')
+            .replace(/ä/g, '_x00e4_')
+            .replace(/Ä/g, '_x00c4_')
+            .replace(/ö/g, '_x00e6_')
+            .replace(/Ö/g, '_x00c6_')
+        );
     }
 
     handleChange = (e) => {
-        this.fetchFromBackend(e.target.value.trim())
+        let inputValue = this.handleInputChars(e.target.value.trim());
+        if(inputValue <1){
+            return;
+        } 
+        this.fetchGamesFromBackend(inputValue);
+        this.fetchStreamersFromBackend(inputValue);
     }
 
     render () {
         if(this.state.gameArray.length>1){
-        }
-        
+
+        }  
         return(
             <div>
                 <Searchbar onChange={this.handleChange}/>
