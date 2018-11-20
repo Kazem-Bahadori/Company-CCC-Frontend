3db6869970f707f29bedf5beae7bc0d9a6566248
JOHAN DE GROOT <johde234@student.liu.se>
2018-11-20 17:55:37 +0100

    Code skeleton for highlighted steam games

--- src/molecules/PopularGame.js
+++ src/molecules/PopularGame.js
@@ -18,32 +18,35 @@ class PopularGame extends Component {
 
   render() {
 
-    if (this.props.steamBool) {
+    if (this.props.steamBool && (this.props.gameId === '29595'|| this.props.gameId === '491487')) {
     return (
-     
-       <div onClick={this.props.onClick} className='popular-game-holder'>
-       <div className="popular-game-image-container">
-       <img className="popular-game-image" src={this.props.image} alt={this.props.gameName} />
-       
-     </div>
-     <div className="game-name-holder">
-       <p className="game-name"> {this.props.gameName}</p>
-       <img className="popular-game-image-logo" src={steamlogo} />
-     </div>
-   </div>
-    );
-    //Checks if the game is Fortnite (highlighted)
-  } else if (this.props.gameId === '33214'){
-    return(
       <div onClick={this.props.onClick} className='highlighted-game'>
         <div className='highlighted-game-img'>
+        <img className="popular-game-image" src={this.props.image} alt={this.props.gameName} />
         <img className="popular-game-image" src={this.props.image} alt={this.props.gameName} />
         
       </div>
       <div className="game-name-holder">
         <p className="game-name"> {this.props.gameName}</p>
+        <img className="popular-game-image-logo-highlight" src={steamlogo} />
       </div>
     </div>
+     
+    );
+    //Checks if the game is Fortnite (highlighted)
+  } else if (this.props.steamBool) {
+    return(
+      <div onClick={this.props.onClick} className='popular-game-holder'>
+      <div className="popular-game-image-container">
+      <img className="popular-game-image" src={this.props.image} alt={this.props.gameName} />
+      
+    </div>
+    <div className="game-name-holder">
+      <p className="game-name"> {this.props.gameName}</p>
+      <img className="popular-game-image-logo" src={steamlogo} />
+    </div>
+  </div>
+      
 
     );
   } else {
